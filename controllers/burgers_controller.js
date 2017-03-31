76cbaa79a8d64c32559270d15d94e54666bcad67
Hero <hkhem927@gmail.com>
2017-03-30 22:04:56 -0400

    sequilized the burger

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('../models')['Burger'];
+
+var models = require('../models');
+var path = require ('path');
+
+router.get('/', function (req, res) {
+  res.redirect('/burgers');
+});
+
+router.get('/burgers', function (req, res) {
+  
+  burger.all()
+
+  .then(function(data){
+    var allBurgers = { burgers: data };
+    res.render('index', allBurgers);
+  })
+
+});
+
+router.post('/burgers/insert', function(req, res){  
+
+  burger.create({burger_name: req.body.burger_name})
+  
+  .then(function(){
+    res.redirect('/burgers');
+  })
+})
+
+router.put('/burgers/update/:id', function(req, res){
+  var id = req.params.id;
+
+  burger.update({
+    devoured: true }, {
+    fields: ['devoured'],
+    where: {id: id}
+  })
+
+  .then(function(data){
+    res.redirect('/burgers');
+  })    
+}) 
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Healthy Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tasty Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Burger', true);
